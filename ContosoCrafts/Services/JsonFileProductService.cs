7f1d57c0e2191a5f611cc4e5b4481e4485191239
Mohamed Momen <m7medmo2men@gmail.com>
2021-02-27 16:52:52 +0200

    initial commit

---
+++ ContosoCrafts/Services/JsonFileProductService.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using ContosoCrafts.Models;
+
+namespace ContosoCrafts.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                // De-serliazing : convert json text to actual objects
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                
+            }
+        }
+    }
+}
